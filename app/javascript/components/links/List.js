7ce7ed3471ba09df74ab11a151b609e73d735390
anasansari157 <anasansari157@gmail.com>
2020-08-22 00:17:10 +0530

    add ability to display list of links

--- app/javascript/components/links/List.js
+++ app/javascript/components/links/List.js
@@ -1,13 +1,39 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from "react";
+import PropTypes from "prop-types";
 class List extends React.Component {
-  render () {
-    return (
-      <React.Fragment>
-      <h1>Links will be appearing soon!!</h1>
-      </React.Fragment>
-    );
-  }
+	displayLinkList = (links) => {
+		return (
+			<div>
+				<h1>Links List</h1>
+				<div className="p-1">
+					{links && links.length ? (
+						<ul className="list-group list-unstyled">
+							{links.map((link, index) => {
+								return (
+									<li key={index}>
+										Link id : {link.id}
+										<br />
+										Link title: {link.title}
+									</li>
+								);
+							})}
+						</ul>
+					) : (
+						<h3>No linkk has been created yet</h3>
+					)}
+				</div>
+			</div>
+		);
+	};
+
+	render() {
+		return (
+			<React.Fragment>
+				<h1>Links will be appearing soon!!</h1>
+				{this.displayLinkList(this.props.links)}
+			</React.Fragment>
+		);
+	}
 }
 
-export default List
+export default List;
