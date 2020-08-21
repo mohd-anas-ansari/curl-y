0b19dfd78b6e697f73848bdbb3f12bcdcae01d01
anasansari157 <anasansari157@gmail.com>
2020-08-22 04:22:37 +0530

    validate source_url for spaces

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,5 @@ class Link < ApplicationRecord
   validates :title, presence: true
   validates :source_url, presence: true
   validates :source_url, format: URI::regexp(%w[http https])
+  validates :source_url, format: { without: /\s/, message: "must contain no spaces" }
 end
