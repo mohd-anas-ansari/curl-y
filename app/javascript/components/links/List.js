69c81062e64755ad23872f21ed9d91e2fe9baaba
anasansari157 <anasansari157@gmail.com>
2020-08-22 00:07:00 +0530

    preparation to display links list is done

---
+++ app/javascript/components/links/List.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class List extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+      <h1>Links will be appearing soon!!</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default List
