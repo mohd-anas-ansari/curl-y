144791376ba4b36fb7895a007a406ba6ac669782
anasansari157 <anasansari157@gmail.com>
2020-08-22 12:03:11 +0530

    add root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root "links#index"
   resources :links, only: [:index, :new, :create, :show, :edit]
   get "/:curl_id" => "links#update"
 
