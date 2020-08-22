638df251cd763fbcbd975a70704a3eb576ddf149
anasansari157 <anasansari157@gmail.com>
2020-08-22 22:52:25 +0530

    define design scaffolding for UI

--- app/javascript/components/links/List.js
+++ app/javascript/components/links/List.js
@@ -41,10 +41,27 @@ class List extends React.Component {
 	render() {
 		let links = this.props.links;
 		return (
-			<div className="container">
-				<div className="row">
+			<div className="bg-light">
+				<div className="hero">
+					<h1>Hero</h1>
+					<h1>Form</h1>
+				</div>
+				<div className="container bg-info list-container shadow-lg p-3 mb-5 bg-white rounded">
+					<p>All list</p>
+					{/* <div className="row">
 					<div className="col-md-10">{this.displayLinkList(links)}</div>
 					<div className="col-md-2">{this.displayAddNewLinkButton()}</div>
+				</div> */}
+					<div className="pinned-list">
+						<h6>Pinned links</h6>
+					</div>
+
+					<div className="all-links-list">
+						<h6>All links</h6>
+					</div>
+				</div>
+				<div className="footer">
+					<h1>Footer</h1>
 				</div>
 			</div>
 		);
