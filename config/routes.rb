a3af0fdc053b5883d43429085eec4db312eac706
anasansari157 <anasansari157@gmail.com>
2020-08-22 11:12:14 +0530

    define route for links#update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :links, only: [:index, :new, :create, :show]
+
+  resources :links, only: [:index, :new, :create, :show, :edit]
+  get "/:curl_id" => "links#update"
+
 end
