59e2a3f3245a130a6bd6a25f572dd61ba2396fac
anasansari157 <anasansari157@gmail.com>
2020-08-23 01:39:40 +0530

    add curl_path and edit_link path

--- app/javascript/utils/Routes.js
+++ app/javascript/utils/Routes.js
@@ -9,3 +9,11 @@ export function new_link_path() {
 export function link_path(id) {
 	return `/links/${id}`;
 }
+
+export function curl_path(curl_id) {
+	return `/${curl_id}`;
+}
+
+export function edit_link_path(id) {
+	return `/links/${id}/edit`;
+}
