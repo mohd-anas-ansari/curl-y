1dd1ec2adee8f85b7fa2995d22fcef823e568492
anasansari157 <anasansari157@gmail.com>
2020-08-23 02:32:11 +0530

    initiate form

---
+++ app/javascript/components/links/Form.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+class Form extends React.Component {
+	render() {
+		return (
+			<React.Fragment>
+				<div class="form-container">
+					<form>
+          
+					</form>
+				</div>
+			</React.Fragment>
+		);
+	}
+}
+
+export default Form;
