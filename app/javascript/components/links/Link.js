649ac18a02006fe8cf2ebc3f06c13bbec0f6a0a7
anasansari157 <anasansari157@gmail.com>
2020-08-25 00:45:56 +0530

    add report button

--- app/javascript/components/links/Link.js
+++ app/javascript/components/links/Link.js
@@ -20,7 +20,7 @@ class Link extends React.Component {
 		API.updateLinkCountAndRedirect(id)
 			.then((response) => {
 				window.open(this.props.link.source_url);
-        window.location.href = "/";
+				window.location.href = "/";
 				// window.location.href = this.props.link.source_url;
 			})
 			.catch((error) => {
@@ -76,7 +76,24 @@ class Link extends React.Component {
 							</svg>
 						)}
 					</span>
-					<h5 className="link-title">{link.title}</h5>
+					<h5 className="link-title">
+						{link.title}{" "}
+						<a className="report-button" href={Routes.link_path(link.id)}>
+							<svg
+								width="1em"
+								height="1em"
+								viewBox="0 0 16 16"
+								class="bi bi-file-earmark-text-fill"
+								fill="currentColor"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M2 3a2 2 0 0 1 2-2h5.293a1 1 0 0 1 .707.293L13.707 5a1 1 0 0 1 .293.707V13a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V3zm7 2V2l4 4h-3a1 1 0 0 1-1-1zM4.5 8a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7zM4 10.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5z"
+								/>
+							</svg>
+						</a>
+					</h5>
 					{/* <a href={Routes.curl_path(link.curl_id)}> */}
 					<button
 						className="curl-url"
