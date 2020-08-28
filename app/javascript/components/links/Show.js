80a8bcbaef3cdf14d73cc3ccb9f95f8bb08e0148
anasansari157 <anasansari157@gmail.com>
2020-08-29 04:29:53 +0530

    use ReportDetails fragment

--- app/javascript/components/links/Show.js
+++ app/javascript/components/links/Show.js
@@ -1,16 +1,11 @@
 import React, { Component } from "react";
 
 import * as Routes from "../../utils/Routes";
+import ReportDetails from "./ReportDetails";
 
 class Show extends Component {
-	formatDate = (dateString) => {
-		const options = { year: "numeric", month: "long", day: "numeric" };
-		return new Date(dateString).toLocaleDateString(undefined, options);
-	};
-
 	render() {
 		const link = this.props.link;
-		const ROOT_URL = "http://curl-y.herokuapp.com";
 		console.log(link, "LINK");
 		return (
 			<React.Fragment>
@@ -20,29 +15,7 @@ class Show extends Component {
 					</h1>
 
 					<div className="p-5 shadow-lg p-3 mb-5 bg-white rounded d-flex">
-						<div className="details">
-							<small className="curls-heading">Curl #{link.id}</small>
-							<h1 className="mt-5 ml-3">{link.title}</h1>
-							<p className="mt-5 ml-3">
-								<span className="mr-4">
-									<strong>CURL LINK:</strong>
-								</span>
-								{`${ROOT_URL}/${link.curl_id}`}
-							</p>
-							<p className="ml-3 mb-5">
-								<span className="mr-4">
-									<strong>SOURCE LINK:</strong>
-								</span>
-								{link.source_url}
-							</p>
-							{/* <p>Curl URL: -> {link.curl_id}</p> */}
-							{/* <p>Click Count: -> {link.click_count}</p> */}
-							{/* <p>Pinned: -> {link.is_pinned ? "Yes" : "No"}</p> */}
-							<small className="ml-3">
-								<span className="date-heading">CREATED ON: </span>
-								{this.formatDate(link.created_at)}
-							</small>
-						</div>
+						<ReportDetails link={link} />
 
 						<div className="count-section d-flex align-items-center flex-column justify-content-center mr-0 w-50">
 							<div>
